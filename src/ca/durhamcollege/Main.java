fcdfd1addc638059149a36b8f2c78c21d56b0966
ASHOK SASITHARAN <ASHOK2801@hotmail.com>
2020-11-13 20:55:07 -0500

    initial commit

---
+++ src/ca/durhamcollege/Main.java
@@ -0,0 +1,16 @@
+package ca.durhamcollege;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args)
+    {
+        //define scanner
+        Scanner keyboard = new Scanner(System.in);
+
+        //Variables
+        String [] playerNames  = {"Jacky", "Ashok", "Tom"};
+      //  int [][] score = new int [][];
+    }
+}
